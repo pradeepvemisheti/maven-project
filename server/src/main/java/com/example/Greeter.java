be2c7e30327d12278414bb7539274a28b137c8db
pradeep <pradeep.vemisheti@gmail.com>
2018-03-28 18:41:22 +0530

    v2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,19 +1,20 @@
 package com.example;
 
 /**
- * This is a class.
- */
+ ** This is a class.
+ **/
+
 public class Greeter {
-
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+	/**
+	 * ** This is a constructor
+	 * **/
+	public Greeter() {
+			 }	    
+	/**
+	 * ** @param someone the name of a person
+	 * ** @return greeting string
+	 * **/
+	public final String greet(final String someone) {
+		return String.format("Hello, %s!", someone);
+	}
 }
